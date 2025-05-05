6dd1fd9d31856a9fff8341072672980e43fc7a55
Victor Eliot <lezhnev.victor@gmail.com>
2025-05-05 19:21:01 +0200

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,7 +5,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    databaseType: "postgres", // ← toto přidej
+    databaseType: "postgres",
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
