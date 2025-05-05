eb6f7c4d3fa80b8e7fc9314610b79ae655f27bd9
Victor Eliot <lezhnev.victor@gmail.com>
2025-05-05 19:22:37 +0200

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -3,10 +3,14 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
+    admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     databaseType: "postgres",
-    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+        redisUrl: process.env.REDIS_URL,
+        workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
